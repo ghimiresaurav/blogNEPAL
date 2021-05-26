0423b4d15f6f9f95f239f6298d7b247f77f0ab2c
Saurav Ghimire <ghimiresaurav09@gmail.com>
2021-05-26 21:44:03 +0545

    testing auth code

---
+++ public/js/dashboard.js
@@ -0,0 +1,12 @@
+// fetch("/dashboard", {
+//   method: "GET",
+//   headers: {
+//     "Content-type": "application/json",
+//     authorization: localStorage.getItem("token"),
+//   },
+// })
+//   .then((response) => response.json())
+//   .then((data) => console.log(data));
+
+// console.log(localStorage.getItem("token"));
+// console.log("worelrjlwej");
