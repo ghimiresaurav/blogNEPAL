89d4593bb028b97fe4c6ca91489f19e205a4e7a4
rishikeshkhakurel <rk31041719@student.ku.edu.np>
2021-06-30 18:03:04 +0000

    search by tag and search done

---
+++ controllers/Search.js
@@ -0,0 +1,18 @@
+const MongoClient = require("mongodb").MongoClient;
+
+module.exports = (req, res) => {
+    const { name } = req.body;
+    console.log(req.body)
+    MongoClient.connect(
+        process.env.DB_URL,
+        { useNewUrlParser: true, useUnifiedTopology: true },
+        async (err, client) => {
+            if (err) throw err;
+            const db = client.db(process.env.DB_NAME);
+            const query = { tags: name };
+            //search tag
+            const posts = await db.collection("blogs").find(query).toArray();
+            return res.json(posts);
+        }
+    );
+};
