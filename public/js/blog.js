7ef1fcf04b052d4ae5eae18c2effff2b4f0669ed
rishikeshkhakurel <rk31041719@student.ku.edu.np>
2021-07-31 11:00:44 +0000

    blog post comment and like done

--- public/js/blog.js
+++ public/js/blog.js
@@ -1,4 +1,3 @@
-console.log("here");
 const blogId = location.href.split("=")[1];
 
 const wrapBlog = (blog) => {
@@ -24,13 +23,119 @@ const wrapBlog = (blog) => {
   document.getElementById("auth-name").innerText = blog.author.name;
   document.getElementById("calendar").innerText = blog.date;
   document.getElementById("blog-body").innerHTML = blog.content;
-  document.getElementById("like-count").innerText = blog.like.length;
-  document.getElementById("comment-count").innerText = blog.comments.length;
+
+  //like comment count section
+  const LikeNo = blog.like.length;
+  const CommentNo = blog.comments.length;
+  const LikeStatus = Likestat(blog.like);
+  let clas = "";
+  let id = "";
+
+  if (LikeStatus) {
+    clas = "fas fa-heart";
+    id = "fill-red";
+  } else {
+    clas = "far fa-heart";
+    id = "fill-none";
+  }
+
+  const likesharecount = document.getElementById("likesharecmt");
+  likesharecount.innerHTML = `
+    <i onclick="Like(${LikeStatus})" class="${clas}" id=${id} style="font-size: 20px"></i>
+    <p id="LikeNo">${LikeNo}</p>
+    <i class="far fa-comment" style="font-size: 20px"></i>
+    <p>${CommentNo}</p>
+    <i class="fas fa-share" style="font-size: 20px"></i>
+    `;
+
+  blog.comments.forEach((data) => {
+    comment(data);
+  });
+};
+
+//render comment
+
+const comment = (comment) => {
+  const cmt_list = document.getElementById("comment-list");
+  const cmt = document.createElement("div");
+  cmt.innerHTML = `
+      <div class="comments">
+                        <div>
+                          <img id="commentimage" src=${comment.user.avatar} alt="pic"/>
+                        </div>
+                        <div class="comment-text">
+                          <p><strong>${comment.user.name} </strong></p>
+                          <p>${comment.body} </p>
+                        </div>
+          
+                      </div>
+    `;
+
+  cmt_list.append(cmt);
+};
+
+//checks if liked or not return true if likes false if not liked so that like color changes
+
+const Likestat = (like) => {
+  const userId = localStorage.getItem("userId");
+  var __FOUND = false;
+  for (var i = 0; i < like.length; i++) {
+    if (like[i].id == userId) {
+      __FOUND = true;
+      break;
+    }
+  }
+  return __FOUND;
+};
+
+//run when like button is pressed and like parameter is send to backend
+const Like = (LikeStatus) => {
+  console.log(LikeStatus);
+  const userId = localStorage.getItem("userId");
+
+  if (LikeStatus) {
+    const LikeIcon = document.getElementById("fill-red");
+    LikeIcon.id = "fill-none";
+    LikeIcon.className = "far fa-heart";
+    LikeIcon.setAttribute("onclick", "Like(false)");
+    const LikeNum = document.getElementById("LikeNo");
+    const number = parseInt(LikeNum.innerHTML) - 1;
+    LikeNum.innerHTML = number;
+  } else {
+    const LikeIcon = document.getElementById("fill-none");
+    LikeIcon.id = "fill-red";
+    LikeIcon.className = "fas fa-heart";
+    LikeIcon.setAttribute("onclick", "Like(true)");
+    const LikeNum = document.getElementById("LikeNo");
+    const number = parseInt(LikeNum.innerHTML) + 1;
+    LikeNum.innerHTML = number;
+  }
+  const fetchOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ postId: blogId, userId: userId }),
+  };
+  const res = fetch("/protected/like", fetchOptions);
+};
+
+//post comment in backend
+const postComment = (e, comment) => {
+  console.log(comment);
+  e.preventDefault();
+  const fetchOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ postId: blogId, comment: comment }),
+  };
+  fetch("/protected/post-comment", fetchOptions);
 };
 
 fetch(`/protected/blog/${blogId}`)
   .then((resp) => resp.json())
   .then((response) => {
-    console.log(response);
     wrapBlog(response);
   });
