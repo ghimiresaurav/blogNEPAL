9bd5e9c7799f5dd77d06191232b1298f8931c726
Saurav Ghimire <ghimiresaurav09@gmail.com>
2021-08-03 15:28:16 +0545

    implement dynamic images-gallery-slideshow

--- public/js/blog.js
+++ public/js/blog.js
@@ -1,18 +1,32 @@
 const blogId = location.href.split("=")[1];
+const row = document.getElementById("row");
+let images, prevSlide;
 
 document.getElementById("user-avatar").src = localStorage.getItem("avatarLink");
 
 const wrapBlog = (blog) => {
-  const images = blog.links.split(", ");
+  images = blog.links.split(", ");
   images.shift();
   if (images.length) {
-    const imageContainer = document.getElementById("card-image");
-    const img = document.createElement("img");
-    img.setAttribute("alt", "blog-image");
-    img.setAttribute("src", images[0]);
-    imageContainer.appendChild(img);
+    max = images.length;
+
+    images.forEach((image, index) => {
+      //create a column (parent) div for each image tag
+      const column = document.createElement("div");
+      column.classList.add("column");
+      //add "active" class to the first slide by default
+      const imgClass = index == 0 ? "demo active" : "demo";
+      column.innerHTML = `<img class="${imgClass}" src="${image}" style="width: 100%" onclick="changeSlide(this, ${
+        index + 1
+      })">`;
+      row.appendChild(column);
+    });
+    //as soon as the first columns(images' parents) are appended to the row div
+    //initialize prevSlide as the first column in the row div
+    prevSlide = row.children[0].firstElementChild;
   }
 
+  //display the blog on the screen
   document.getElementById("blog-title").innerText = blog.title;
   document.getElementById("auth-avatar").firstElementChild.src =
     blog.author.avatar;
@@ -47,6 +61,7 @@ const wrapBlog = (blog) => {
   blog.comments.forEach((data) => {
     comment(data);
   });
+  showSlides(slideIndex);
 };
 
 //render comment
@@ -55,19 +70,16 @@ const comment = (comment) => {
   const cmt_list = document.getElementById("comment-list");
   const cmt = document.createElement("div");
   cmt.innerHTML = `
-      <div class="comments">
-                        <div>
-                          <img id="commentimage" src=${comment.user.avatar} alt="pic"/>
-                        </div>
-                        <div class="comment-text">
-                          <p><strong>${comment.user.name} </strong>
-                          <span class="comment-time">${comment.date}</span></p>
-                          <p>${comment.body} </p>
-                        </div>
-          
-                      </div>
-    `;
-
+    <div class="comments">
+      <div>
+        <img id="commentimage" src=${comment.user.avatar} alt="pic"/>
+      </div>
+      <div class="comment-text">
+        <p><strong>${comment.user.name} </strong>
+        <span class="comment-time">${comment.date}</span></p>
+        <p>${comment.body} </p>
+      </div>
+    </div>`;
   cmt_list.append(cmt);
 };
 
@@ -138,34 +150,77 @@ fetch(`/protected/blog/${blogId}`)
 
 // for image carousel
 let slideIndex = 1;
-showSlides(slideIndex);
 
-function plusSlides(n) {
-  showSlides((slideIndex += n));
+function showSlides(n) {
+  const image = document.getElementById("blog-image"),
+    max = images.length;
+
+  if (n > max) {
+    slideIndex = 1;
+  } else if (n < 1) {
+    slideIndex = max;
+  }
+
+  image.src = images[slideIndex - 1];
+  document.getElementById("numbertext").innerText = `${slideIndex} / ${max}`;
 }
 
 function currentSlide(n) {
   showSlides((slideIndex = n));
 }
 
-function showSlides(n) {
-  let i,
-    slides = document.getElementsByClassName("blogImages"),
-    dots = document.getElementsByClassName("demo"),
-    captionText = document.getElementById("caption");
-  if (n > slides.length) {
-    slideIndex = 1;
+const changeSlide = (nextSlide, n) => {
+  prevSlide.classList.remove("active");
+  nextSlide.classList.add("active");
+
+  prevSlide = nextSlide;
+  currentSlide(n);
+};
+
+function plusSlides(n) {
+  //get all the column elements
+  const columns = Array.from(row.children);
+  //filter out the column element with "active" class
+  const activeColumn = columns.filter((item) =>
+    item.firstElementChild.classList.contains("active")
+  )[0];
+
+  //get the image div of the active column with "activeColumn.firstElementChild"
+  //get it's onclick attribute which is "changeSlide(this, i)", a string ofc
+  //split it with ", " to get ["changeSlide(this", "i)"]
+  //take the first element(i.e. "i)") and disregard the zeroth
+  //split it again with ")" to get  ["i", ""]
+  //take the zeroth element(i.e. "i") and parse it with parseInt() function
+  //finally currentSlideIndex = i (int)
+
+  const currentSlideIndex = parseInt(
+    activeColumn.firstElementChild
+      .getAttribute("onclick")
+      .split(", ")[1]
+      .split(")")[0]
+  );
+
+  let nextSlide;
+
+  if (n == 1) {
+    //the user clicked on next slide button
+    if (currentSlideIndex == columns.length) {
+      //the current slide on the screen is the last slide
+      nextSlide = columns[0].firstElementChild;
+      changeSlide(nextSlide, (slideIndex += n));
+    } else {
+      nextSlide = activeColumn.nextElementSibling.firstElementChild;
+      changeSlide(nextSlide, (slideIndex += n));
+    }
+  } else if (n == -1) {
+    //the user clicked on previous slide button
+    if (currentSlideIndex == 1) {
+      //the current slide on the screen is the first slide
+      nextSlide = columns[columns.length - 1].firstElementChild;
+      changeSlide(nextSlide, (slideIndex += n));
+    } else {
+      nextSlide = activeColumn.previousElementSibling.firstElementChild;
+      changeSlide(nextSlide, (slideIndex += n));
+    }
   }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-  captionText.innerHTML = dots[slideIndex - 1].alt;
 }
